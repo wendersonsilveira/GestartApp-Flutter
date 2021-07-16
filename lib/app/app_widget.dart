2d3806f29678720a51bf4cab3836b494156f9f64
Helder <h.alcantara8@hotmail.com>
2021-07-16 14:11:07 -0300

    Correção linguagem

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -19,6 +19,7 @@ class _AppWidgetState extends ModularState<AppWidget, AppController> {
       DeviceOrientation.portraitDown,
     ]);
     controller.isUserLogged();
+
     return Observer(builder: (_) {
       return controller.loading
           ? Container(child: Center(child: CircularProgressCustom()))
@@ -28,9 +29,8 @@ class _AppWidgetState extends ModularState<AppWidget, AppController> {
                 GlobalWidgetsLocalizations.delegate,
                 GlobalCupertinoLocalizations.delegate,
               ],
-              supportedLocales: [
-                const Locale('en', 'US'),
-                const Locale('pt_BR', 'BR'),
+              supportedLocales: <Locale>[
+                Locale.fromSubtags(languageCode: 'en'),
               ],
               navigatorKey: Modular.navigatorKey,
               title: 'Gestart App',
