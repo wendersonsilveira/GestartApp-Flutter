18b0eb5d2c28ace3538dc3e74e448ef4834c6ed7
Wenderson Silveira <wenderson@gestartcondominios.com.br>
2022-05-10 09:32:18 -0300

    ajuste menu bottom

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -77,6 +77,14 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
               ),
               BottomNavigationBarItem(
                 icon: Icon(
+                  Icons.info,
+                  color: Colors.grey,
+                ),
+                label: '',
+                backgroundColor: AppColorScheme.corMenuBotton,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
                   FlutterIcons.mic_mdi,
                   color: Colors.grey,
                 ),
